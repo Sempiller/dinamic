8a20f550aa69ef9592309af2de97e0ab2c3445fd
sempiller <110603660+Sempiller@users.noreply.github.com>
2025-06-28 22:48:00 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Dinamic",
+        Text = "Use a executor that has more than 70 sUNC otherwise it wont work perfectly.",
+        Duration = 5
+})
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Dinamic",
+        Text = "Xeno/Xeno based executors are not supported due to MoonSec.",
+        Duration = 5
+})
+
 local placeId = game.PlaceId
 
 if placeId == 5041144419 then
@@ -8,7 +20,7 @@ elseif placeId == 662417684 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Sempiller/dinamic/refs/heads/main/luckyblock.lua"))()
 else
     game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Dynamic",
+        Title = "Dinamic",
         Text = "This game isn't supported yet.",
         Duration = 5
     })
